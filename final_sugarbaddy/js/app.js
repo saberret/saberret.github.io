69d76966ba4e45ef89f3c4d387ce3337942515b1
saberret <hi.its.yin@gmail.com>
2017-08-21 16:59:52 -0400

    more final edits

---
+++ final_sugarbaddy/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+	$(window).scroll(function () {
+		// console.log('scrolling');
+		console.log($(window).scrollTop());
+	
+		if ($(window).scrollTop() > 680) {
+			$('nav-cate-course').replaceWith('site-nav-fixed');
+		} else {
+			$('nav-cate-course').remove('site-nav-fixed');
+		}
+
+
+	})
+
+})
