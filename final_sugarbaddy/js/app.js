5bd3450fbfe6c673c5d6b0a98c696b781c20c7f0
saberret <hi.its.yin@gmail.com>
2017-08-31 02:31:01 -0400

    more fixes

--- final_sugarbaddy/js/app.js
+++ final_sugarbaddy/js/app.js
@@ -1,16 +1,43 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-// 	$(window).scroll(function () {
-// 		// console.log('scrolling');
-// 		console.log($(window).scrollTop());
+	$(window).scroll(function () {
+		// console.log('scrolling');
+		console.log($(window).scrollTop());
 	
-// 		if ($(window).scrollTop() > 680) {
-// 			$('nav-cate-course').replaceWith('site-nav-fixed');
-// 		} else {
-// 			$('nav-cate-course').remove('site-nav-fixed');
-// 		}
+		if ($(window).scrollTop() > 852) {
+			$('site-nav').addClass('fixed');
+			$('sticky-pics').addClass('fixed');
+			$('sticky-home').addClass('fixed');
+			$('sticky-meal-course').addClass('fixed');
+			$('sticky-low-carb').addClass('fixed');
+			$('sticky-quick-easy').addClass('fixed');
+			$('sticky-budget-friendly').addClass('fixed');
+
+		} else {
+			$('site-nav').removeClass('fixed');
+			$('sticky-pics').removeClass('fixed');
+			$('sticky-home').removeClass('fixed');
+			$('sticky-meal-course').removeClass('fixed');
+			$('sticky-low-carb').removeClass('fixed');
+			$('sticky-quick-easy').removeClass('fixed');
+			$('sticky-budget-friendly').removeClass('fixed');
+		}
+	})
 
 
-// 	})
 
-// })
+   	$('a[href*=#]').bind('click', function(e) {
+		e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+	       
+		var target = $(this).attr("href"); //Get the target
+				
+		// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({ scrollTop: $(target).offset().top}, 1000, function() {
+		     location.hash = target + 800;  //attach the hash (#jumptarget) to the pageurl
+		});
+				
+		return false;
+   
+	});
+
+})
