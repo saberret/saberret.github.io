73731fd6e05d80500f0471fc821a1933598e19be
saberret <hi.its.yin@gmail.com>
2017-09-29 06:23:33 -0400

    done with life

--- final_sugarbaddy/js/app.js
+++ final_sugarbaddy/js/app.js
@@ -1,28 +1,38 @@
 $(document).ready(function () {
 
+	$('.logo-wrap').mouseenter(function () {
+		$('.logo-change').css('visibility', 'visible');
+		$('.logo').hide();
+	});	
+	$('.logo-wrap').mouseleave(function () {
+		$('.logo-change').css('visibility', 'hidden');
+		$('.logo').show();
+	});
+	
+
 	$(window).scroll(function () {
 		// console.log('scrolling');
 		console.log($(window).scrollTop());
 	
-		if ($(this).scrollTop() > 852) {
-			$('site-nav').addClass('fixed');
-			$('sticky-pics').addClass('fixed');
-			$('sticky-home').addClass('fixed');
-			$('sticky-meal-course').addClass('fixed');
-			$('sticky-low-carb').addClass('fixed');
-			$('sticky-quick-easy').addClass('fixed');
-			$('sticky-budget-friendly').addClass('fixed');
-			$('sticky-question').addClass('fixed');
+		if ($(this).scrollTop() > 800) {
+			$('.site-nav').addClass('fixed');
+			$('.sticky-pics').addClass('fixed');
+			$('.sticky-home').addClass('fixed');
+			$('.sticky-meal-course').addClass('fixed');
+			$('.sticky-low-carb').addClass('fixed');
+			$('.sticky-quick-easy').addClass('fixed');
+			$('.sticky-budget-friendly').addClass('fixed');
+			$('.sticky-question').addClass('fixed');
 
 		} else {
-			$('site-nav').removeClass('fixed');
-			$('sticky-pics').removeClass('fixed');
-			$('sticky-home').removeClass('fixed');
-			$('sticky-meal-course').removeClass('fixed');
-			$('sticky-low-carb').removeClass('fixed');
-			$('sticky-quick-easy').removeClass('fixed');
-			$('sticky-budget-friendly').removeClass('fixed');
-			$('sticky-question').removeClass('fixed');
+			$('.site-nav').removeClass('fixed');
+			$('.sticky-pics').removeClass('fixed');
+			$('.sticky-home').removeClass('fixed');
+			$('.sticky-meal-course').removeClass('fixed');
+			$('.sticky-low-carb').removeClass('fixed');
+			$('.sticky-quick-easy').removeClass('fixed');
+			$('.sticky-budget-friendly').removeClass('fixed');
+			$('.sticky-question').removeClass('fixed');
 		}
 	})
 
